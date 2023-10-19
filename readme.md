14747ee8ea28103798502f36312053b0f70fd7a1
wjamilaion <wjamil@aiondigital.com>
2023-10-19 17:33:46 +0500

    node js auto instrumentation

---
+++ readme.md
@@ -0,0 +1,49 @@
+
+# @aiondigital/opentelemetry
+
+## introduction
+
+`@aiondigital/opentelemetry` is an npm package for setting up OpenTelemetry tracing and metrics in Node.js applications. It provides an easy way to configure and initialize OpenTelemetry instrumentation and exporters.
+
+
+
+## Installation
+
+
+You can install this package using npm or yarn:
+
+```bash
+npm install @aiondigital/opentelemetry
+```
+    
+## Usage/Examples
+
+To use this package, you can follow these steps:
+
+1. install the package
+```
+npm install @aiondigital/opentelemetry
+```
+2. import and setup
+```
+const opentelemetry = require('@aiondigital/opentelemetry');
+opentelemetry.setup();
+```
+3. Configuration
+
+You can configure the behavior of the package using environment variables. Here are the available configuration options:
+
+- `ENV_RBX_TELEMETRY_ENABLED`: Enable or disable telemetry (true/false).
+- `ENV_RBX_TELEMETRY_DEBUG_ENABLED`: Enable debug mode (true/false).
+- `ENV_RBX_APP_NAME`: Specify the application name.
+- `ENV_RBX_JAEGER_URL`: Specify the Jaeger endpoint.
+- `ENV_RBX_JAEGER_TOKEN`: Specify the Jaeger token.
+- `ENV_RBX_PROMETHEUS_ENABLED`: Enable or disable Prometheus metrics (true/false).
+- `ENV_RBX_PROMETHEUS_SCRAPPER_ENDPOINT`: Specify the Prometheus scrape endpoint.
+- `ENV_RBX_PROMETHEUS_SCRAPPER_PORT`: Specify the Prometheus scrape port.
+
+Make sure to set these environment variables as needed.
+## License
+
+This package is released under the [MIT License](https://choosealicense.com/licenses/mit/) 
+
---
+++ src/instrumentation.ts
@@ -0,0 +1,198 @@
+import {
+  NodeTracerProvider,
+  BatchSpanProcessor,
+  SamplingResult,
+  ParentBasedSampler,
+  TraceIdRatioBasedSampler,
+} from '@opentelemetry/sdk-trace-node';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { ExpressInstrumentation } from 'opentelemetry-instrumentation-express';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { MeterProvider } from '@opentelemetry/sdk-metrics';
+import { Context, SpanKind, Attributes, Link } from '@opentelemetry/api';
+
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+
+import * as fs from 'fs';
+
+/**
+ * Retrieves the application name from the 'package.json' file.
+ * @returns The application name or undefine if there is an error.
+ */
+const applicationName = () => {
+  try {
+    const data = fs.readFileSync('./package.json', 'utf8');
+    const packageInfo: { name: string } = JSON.parse(data);
+    return packageInfo.name;
+  } catch (err) {
+    console.error('Error:', err);
+    return;
+  }
+};
+
+// Configuration for Prometheus.
+interface PrometheusConfig {
+  enabled: boolean;
+  endpoint: string;
+  port: number;
+}
+
+// Configuration for Telemetry.
+interface TelemetryConfig {
+  enabled: boolean;
+  debug: boolean;
+  appName?: string;
+  jaeger?: string;
+  jaeger_token?: string;
+  prometheus: PrometheusConfig;
+}
+
+/**
+ * Retrieves the telemetry configuration.
+ * @returns The telemetry configuration.
+ */
+const telemetryConfig = (): TelemetryConfig => {
+  return {
+    enabled: process.env.ENV_RBX_TELEMETRY_ENABLED == 'true',
+    debug: process.env.ENV_RBX_TELEMETRY_DEBUG_ENABLED == 'true',
+    appName: process.env.ENV_RBX_APP_NAME || applicationName(),
+    jaeger: process.env.ENV_RBX_JAEGER_URL,
+    jaeger_token: process.env.ENV_RBX_JAEGER_TOKEN,
+    prometheus: {
+      enabled: process.env.ENV_RBX_PROMETHEUS_ENABLED == 'true',
+      endpoint:
+        process.env.ENV_RBX_PROMETHEUS_SCRAPPER_ENDPOINT ||
+        PrometheusExporter.DEFAULT_OPTIONS.endpoint,
+      port: Number.parseInt(
+        process.env.ENV_RBX_PROMETHEUS_SCRAPPER_PORT || '0',
+      ),
+    },
+  };
+};
+
+/**
+ * Sets up telemetry with OpenTelemetry.
+ */
+export default function setup() {
+  const config = telemetryConfig();
+
+  // Do traces only if it's enabled.
+  if (!config.enabled) {
+    return;
+  }
+
+  // Debug OpenTelemetry only if it's enabled.
+  if (config.debug) {
+    diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+  }
+
+  setupNodeTracer(config);
+  const meterProvider = metricsProvider(config);
+
+  const instrumentation = new NestInstrumentation();
+
+  registerInstrumentations({
+    meterProvider: meterProvider,
+    instrumentations: [
+      instrumentation,
+      getNodeAutoInstrumentations({
+        '@opentelemetry/instrumentation-fs': { enabled: false },
+        '@opentelemetry/instrumentation-net': { enabled: false },
+        '@opentelemetry/instrumentation-dns': { enabled: false },
+        '@opentelemetry/instrumentation-express': { enabled: false },
+        '@opentelemetry/instrumentation-dataloader': { enabled: false },
+      }),
+      new ExpressInstrumentation(),
+    ],
+  });
+}
+
+/**
+ * Sets up the Node.js tracer with OpenTelemetry.
+ * @param config - The telemetry configuration.
+ */
+function setupNodeTracer(config: TelemetryConfig) {
+  const serviceName = config.appName;
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+    sampler: new RubixSampler({
+      root: new TraceIdRatioBasedSampler(0.5),
+    }),
+  });
+
+  provider.register();
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(
+      new OTLPTraceExporter({
+        url: config.jaeger,
+        headers: {
+          Authorization: config.jaeger_token,
+        },
+      }),
+    ),
+  );
+}
+
+/**
+ * Sets up the metrics provider for Prometheus.
+ * @param config - The telemetry configuration.
+ * @returns The metrics provider.
+ */
+function metricsProvider(config: TelemetryConfig) {
+  const { prometheus } = config;
+
+  if (!prometheus.enabled) {
+    return;
+  }
+
+  const metricExporter = new PrometheusExporter(
+    {
+      endpoint: prometheus.endpoint,
+      port: prometheus.port,
+    },
+    () => {
+      console.log(
+        `Prometheus scrape endpoint: http://localhost:${prometheus.port}${prometheus.endpoint}`,
+      );
+    },
+  );
+
+  // Creates MeterProvider and installs the exporter as a MetricReader
+  const meterProvider = new MeterProvider();
+  meterProvider.addMetricReader(metricExporter);
+  return meterProvider;
+}
+
+// Custom sampler for tracing (can be extended as per requirements)
+class RubixSampler extends ParentBasedSampler {
+  shouldSample(
+    context: Context,
+    traceId: string,
+    spanName: string,
+    spanKind: SpanKind,
+    attributes: Attributes,
+    links: Link[],
+  ): SamplingResult {
+    // if (attributes['http.url'] ?? "".includes(`${port}${endpoint}`)) {
+    //     return {
+    //         decision: SamplingDecision.NOT_RECORD
+    //     }
+    // }
+    return super.shouldSample(
+      context,
+      traceId,
+      spanName,
+      spanKind,
+      attributes,
+      links,
+    );
+  }
+}
